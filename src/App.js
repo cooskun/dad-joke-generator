955004d0c256fc42c57ac208ba70b6b7909756ae
coskunbaris <coskun.bris@gmail.com>
2020-07-18 23:41:49 +0200

    Create config file for Prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import RandomJoke from "./components/RandomJoke";
-import SearchJoke from "./components/SearchJoke";
+import React from 'react'
+import RandomJoke from './components/RandomJoke'
+import SearchJoke from './components/SearchJoke'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <RandomJoke />
       <SearchJoke />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
