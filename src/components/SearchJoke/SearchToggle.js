66b8ca234751e6f559992b7371cb314bfc136c43
coskunbaris <coskun.bris@gmail.com>
2020-07-20 00:14:13 +0200

    Replace current render logic with React Router in search area

--- src/components/SearchJoke/SearchToggle.js
+++ src/components/SearchJoke/SearchToggle.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 import { FaTimes } from 'react-icons/fa'
 import { GoSearch } from 'react-icons/go'
 import styles from './SearchToggle.module.scss'
 
-const SearchToggle = ({ enable, onClick }) => {
+const SearchToggle = ({ onExit }) => {
   return (
     <div className={styles.toggler}>
-      {enable ? (
-        <FaTimes onClick={onClick} className={styles.icon} />
-      ) : (
-        <GoSearch onClick={onClick} className={styles.icon} />
-      )}
+      <Switch>
+        <Route exact path="/">
+          <Link to="/search">
+            <GoSearch className={styles.icon} />
+          </Link>
+        </Route>
+
+        <Route path="/search">
+          <FaTimes onClick={onExit} className={styles.icon} />
+        </Route>
+      </Switch>
     </div>
   )
 }
